eba5889c955f3044f340110d023aeed569dbb330
Sergey <sergio.mipt@gmail.com>
2017-07-04 01:06:44 +0300

    third commit

--- svm_linear.py
+++ svm_linear.py
@@ -1,50 +1,63 @@
 import numpy as np
-import cvxopt
+import copy
+from tqdm import tqdm
+import time
 
 # kernel = 'linear' or 'rbf' or 'poly'
 # labels y have to be -1 or +1
 
 
-class Svm(object):
+class LinearSvm(object):
     def __init__(self, C=1.0, mini_batch_size=None, max_iter=1000, learning_rate=0.001, epsilon=0.00001,
-                 check_stopping_condition=False):
+                 check_stopping_condition=False, random_seed=None):
         self.max_iter = max_iter
         self.learning_rate = learning_rate
         self.C = C
         self.mini_batch_size = mini_batch_size
         self.epsilon = epsilon
         self.check_stopping_conditions = check_stopping_condition
+        self.random_seed = random_seed
 
     # I use (sub) gradient decent method to learn parameters of the linear model (linear SVM) if mini_batch_size is None
     # or stochastic (sub) gradient decent otherwise
     def train(self, X, y):
-        if self.mini_batch_size is None:
-            self.mini_batch_size = X.shape[0]
-        self.weights = np.random.randn(X.shape[1])
-        self.bias = np.random.randn(1)
-        self.loss = []
-        self.loss.append(self._compute_loss(X, y))
+        start_time = time.clock()
+        self._init(X, y)
 
-        for i in range(self.max_iter):
+        for i in tqdm(range(self.max_iter)):
             weights_prev = np.copy(self.weights)
             bias_prev = np.copy(self.bias)
 
             if self.mini_batch_size == X.shape[0]:
                 self._weights_bias_update(X, y)
             else:
-                X_batch, y_batch = self._sample_mini_batch(X, y, self.mini_batch_size)
+                X_batch, y_batch = self._sample_mini_batch()
                 self._weights_bias_update(X_batch, y_batch)
             self.loss.append(self._compute_loss(X, y))
+
+            if self.loss[-1] < self.loss_min:
+                self.loss_min = self.loss[-1]
+                self.weights_min = copy.copy(self.weights)
+                self.bias_min = copy.copy(self.bias)
+                self.iteration_min = i+1
 
             if self.check_stopping_conditions:
                 if self._stopping_conditions(self.loss[-1], self.loss[-2], np.hstack((self.weights, self.bias)),
                                              np.hstack((weights_prev, bias_prev))):
                     break
+        self._set_weights_with_min_loss()
+        finish_time = time.clock()
+        print('Training time:', finish_time - start_time)
 
-    # we want mini_batch to have equal number of elements with label -1 and +1
-    # in case of odd mini_batch_size (excluding 1) number of elements differs on 1 (size // 2 and size // 2 + size % 2)
-    # TODO: check if mini_batch size // 2 is lower then the number of elements in the smallest class
-    # TODO: add training by epochs like in deep learning
+    # we want a mini_batch to have equal number of elements with labels -1 and +1
+    # TODO: add errors handling
+    def _set_weights_with_min_loss(self):
+        self.weights = self.weights_min
+        self.bias = self.bias_min
+
+    def predict(self, X):
+        return np.sign(np.matmul(X, self.weights) + self.bias)
+
     def _stopping_conditions(self, loss_cur, loss_prev, parameters_cur, parameters_prev):
         if np.abs(loss_cur - loss_prev) < self.epsilon:
             return True
@@ -53,17 +66,39 @@ class Svm(object):
 
         return False
 
-    def _sample_mini_batch(self, X, y, size):
-        y_neg = y[y == -1]
-        X_neg = X[y == -1]
-        inds_neg = np.random.choice(X_neg.shape[0], size // 2, replace=False)
+    def _init(self, X, y):
+        if self.mini_batch_size is None:
+            self.mini_batch_size = X.shape[0]
+        else:
+            self.y_neg = y[y == -1]
+            self.X_neg = X[y == -1]
+            self.y_pos = y[y == 1]
+            self.X_pos = X[y == 1]
 
-        y_pos = y[y == 1]
-        X_pos = X[y == 1]
-        inds_pos = np.random.choice(X_pos.shape[0], size // 2 + size % 2, replace=False)
+            if len(self.y_neg) < len(self.y_pos):
+                if len(self.y_neg) < self.mini_batch_size // 2:
+                    print('Error: half size of a mini batch is greater then number of negative samples in the dataset')
+            else:
+                if len(self.y_pos) < self.mini_batch_size // 2:
+                    print('Error: half size of a mini batch is greater then number of positive samples in the dataset')
 
-        X_batch = np.vstack((X_neg[inds_neg], X_pos[inds_pos]))
-        y_batch = np.concatenate((y_neg[inds_neg], y_pos[inds_pos]))
+        if self.random_seed is not None:
+            np.random.seed(self.random_seed)
+        self.weights = np.random.randn(X.shape[1])
+        self.bias = np.random.randn(1)
+        self.loss = []
+        self.loss.append(self._compute_loss(X, y))
+        self.loss_min = self.loss[-1]
+        self.weights_min = self.weights
+        self.bias_min = self.bias
+        self.iteration_min = 0
+
+    def _sample_mini_batch(self):
+        inds_neg = np.random.choice(self.X_neg.shape[0], self.mini_batch_size // 2, replace=False)
+        inds_pos = np.random.choice(self.X_pos.shape[0], self.mini_batch_size // 2, replace=False)
+
+        X_batch = np.vstack((self.X_neg[inds_neg], self.X_pos[inds_pos]))
+        y_batch = np.concatenate((self.y_neg[inds_neg], self.y_pos[inds_pos]))
 
         return X_batch, y_batch
 
@@ -100,16 +135,13 @@ class Svm(object):
         else:
             return 0
 
-    def _subgrad_hinge_loss(self, x, randomized=True, sub_grad_deterministic=-0.5):
+    def _subgrad_hinge_loss(self, x):
         if x < 1:
             return -1
         if x > 1:
             return 0
         if x == 1:
-            if randomized:
-                return np.random.uniform(-1, 0)
-            else:
-                return sub_grad_deterministic
+            return -1
 
     def get_margins(self, X, y):
         return (np.matmul(X, self.weights) + self.bias) * y
---
+++ svm_tests.py
@@ -0,0 +1,115 @@
+from svm_linear import LinearSvm
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
+
+
+def plot_dataset(X, y):
+    print('Dataset:')
+    x_lim_lower = np.min(X[:, 0]) - 1
+    x_lim_upper = np.max(X[:, 0]) + 1
+    y_lim_lower = np.min(X[:, 1]) - 1
+    y_lim_upper = np.max(X[:, 1]) + 1
+    plt.xlim([x_lim_lower, x_lim_upper])
+    plt.ylim([y_lim_lower, y_lim_upper])
+    plt.scatter(X[y == 1, 0], X[y == 1, 1], c='r')
+    plt.scatter(X[y == -1, 0], X[y == -1, 1], c='b')
+    plt.show()
+    return [x_lim_lower, x_lim_upper, y_lim_lower, y_lim_upper]
+
+
+def print_model_summary(y, preds, weights, bias, brief=False):
+    if brief:
+        print('accuracy:', sum(preds == y) / len(y))
+    else:
+        print('predictions:', preds)
+        print('accuracy:', sum(preds == y) / len(y))
+        print('weights:', weights)
+        print('bias:', bias)
+
+
+def linearSVC_model(X, y, C=1.0, random_state=None, brief=False):
+    print('LinearSVC model:')
+    model = LinearSVC(C=C, loss='hinge', random_state=random_state)
+    model.fit(X, y)
+    print_model_summary(y, model.predict(X), model.coef_[0], model.intercept_, brief=brief)
+    return model
+
+
+def SVC_model(X, y, C=1.0, brief=False):
+    print('SVC model:')
+    model = SVC(C=C, kernel='linear')
+    model.fit(X, y)
+    print_model_summary(y, model.predict(X), model.coef_[0], model.intercept_, brief=brief)
+    return model
+
+
+def my_model(X, y, C=1.0, learning_rate=0.001, max_iter=10000, random_seed=10, mini_batch_size=None, brief=False):
+    print('My model:')
+    model = LinearSvm(C=C, learning_rate=learning_rate, max_iter=max_iter,
+                      random_seed=random_seed, mini_batch_size=mini_batch_size)
+    model.train(X, y)
+    print_model_summary(y, model.predict(X), model.get_weights(), model.get_bias(), brief=brief)
+    return model
+
+
+def plot_hyperplane(X, y, model, library, xy_lims):
+    x_lim_lower, x_lim_upper, y_lim_lower, y_lim_upper = xy_lims[0], xy_lims[1], xy_lims[2], xy_lims[3]
+    if library == 'sklearn':
+        w1, w2 = model.coef_[0]
+        bias = model.intercept_
+    if library == 'my':
+        w1, w2 = model.get_weights()
+        bias = model.get_bias()
+    x_plot = np.linspace(x_lim_lower, x_lim_upper, 1000)
+    y_plot = (-x_plot * w1 - bias) / w2
+    plt.xlim([x_lim_lower, x_lim_upper])
+    plt.ylim([y_lim_lower, y_lim_upper])
+    plt.scatter(X[y == 1, 0], X[y == 1, 1], c='r')
+    plt.scatter(X[y == -1, 0], X[y == -1, 1], c='b')
+    plt.plot(x_plot, y_plot)
+    plt.show()
+    return x_plot, y_plot
+
+
+def plot_hplanes_comparison(X, y, x_lin, y_lin, x_svc, y_svc, x_my, y_my, xy_lims):
+    print('Models Comparison:')
+    x_lim_lower, x_lim_upper, y_lim_lower, y_lim_upper = xy_lims[0], xy_lims[1], xy_lims[2], xy_lims[3]
+    plt.xlim([x_lim_lower, x_lim_upper])
+    plt.ylim([y_lim_lower, y_lim_upper])
+    plt.plot(x_my, y_my, color='black', label='my_model')
+    plt.plot(x_svc, y_svc, color='orange', label='SVC')
+    plt.plot(x_lin, y_lin, color='green', label='linearSVC')
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.1)
+    plt.scatter(X[y == 1, 0], X[y == 1, 1], c='r')
+    plt.scatter(X[y == -1, 0], X[y == -1, 1], c='b')
+    plt.show()
+
+
+def svm_test_2d(X, y, C=1.0, learning_rate=0.001, max_iter=10000, random_seed=10,
+                random_state=None, mini_batch_size=None):
+    xy_lims = plot_dataset(X, y)
+
+    model_linSVC = linearSVC_model(X, y, C=C, random_state=random_state)
+    x_plot_lin, y_plot_lin = plot_hyperplane(X, y, model_linSVC, 'sklearn', xy_lims)
+
+    model_SVC = SVC_model(X, y, C=C)
+    x_plot_svc, y_plot_svc = plot_hyperplane(X, y, model_SVC, 'sklearn', xy_lims)
+
+    model_my = my_model(X, y, C=C, learning_rate=learning_rate, max_iter=max_iter,
+                        random_seed=random_seed, mini_batch_size=mini_batch_size)
+    x_plot_my, y_plot_my = plot_hyperplane(X, y, model_my, 'my', xy_lims)
+
+    plot_hplanes_comparison(X, y, x_plot_lin, y_plot_lin, x_plot_svc, y_plot_svc, x_plot_my, y_plot_my, xy_lims)
+
+    return model_linSVC, model_SVC, model_my
+
+
+def svm_test(X, y, C=1.0, learning_rate=0.001, max_iter=10000, random_seed=10,
+             random_state=None, mini_batch_size=None, brief=False):
+    model_linSVC = linearSVC_model(X, y, C=C, random_state=random_state, brief=brief)
+    model_SVC = SVC_model(X, y, C=C, brief=brief)
+    model_my = my_model(X, y, C=C, learning_rate=learning_rate, max_iter=max_iter,
+                        random_seed=random_seed, mini_batch_size=mini_batch_size, brief=brief)
+    return model_linSVC, model_SVC, model_my
